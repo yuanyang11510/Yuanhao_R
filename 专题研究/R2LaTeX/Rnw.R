857d82879962daac4767da42779825b1a08322cf
yuanyang11510 <yuanyang11510@gmail.com>
2025-05-18 17:07:03 +0200

    add contents

---
+++ 专题研究/R2LaTeX/Rnw.R
@@ -0,0 +1,9 @@
+#rfr 可以参考：https://tex.stackexchange.com/questions/25575/how-can-i-use-a-table-generated-by-r-in-latex
+library(knitr)
+
+knit("./Rnw/test.Rnw","./Tex/test1.tex") # 仅生成tex，输出路径扩展名设置为.tex
+knit2pdf("./Rnw/test.Rnw","./Tex/test2.tex",compiler = "xelatex") # 生成tex和pdf，输出路径扩展名设置为.tex
+rnw2pdf("./Rnw/test.Rnw","./Tex/test3.pdf") # 仅生成pdf，输出路径扩展名设置为.pdf，默认使用xelatex编译
+
+
+
